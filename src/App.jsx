4d6ed88277cabe7fc643d90320474550c5a3bca6
Tatiana <tati14953@gmail.com>
2024-01-23 21:02:30 -0300

    v.2 responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,24 @@
-import React from 'react';
-import { NavBar } from './Components/NavBar';
-import { AboutMeSection } from './Components/AboutMeSection';
-import { ProjectsSection } from './Components/ProjectsSection';
-import { TechnologiesSection } from './Components/TechnologiesSection';
-import { ContactForm } from './Components/ContactSection';
-import { Footer } from './Components/Footer';
-import './App.css';
+import { NavBar } from './Components/NavBar'
+import {AboutMeSection} from './Components/AboutMeSection'
+import { ProjectsSection } from './Components/ProjectsSection'
+import {TechnologiesSection} from './Components/TechnologiesSection'
+import {ContactForm } from './Components/ContactSection'
+import {Footer } from './Components/Footer'
+import './App.css'
+
 
 function App() {
-  const props = useSpring({
-    opacity: 1,
-    from: { opacity: 0 },
-  });
 
   return (
     <>
       <NavBar />
-      <animated.div style={props}>
-        <AboutMeSection />
-      </animated.div>
-      <animated.div style={props}>
-        <ProjectsSection />
-      </animated.div>
-      <animated.div style={props}>
-        <TechnologiesSection />
-      </animated.div>
-      <animated.div style={props}>
-        <ContactForm />
-      </animated.div>
-      <animated.div style={props}>
-        <Footer />
-      </animated.div>
+      <AboutMeSection />
+      <ProjectsSection />
+      <TechnologiesSection />
+      <ContactForm />
+      <Footer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
